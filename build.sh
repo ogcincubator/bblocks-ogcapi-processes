fb193b866eb772122155ace3190b6aee993df451
Rob Atkinson <ratkinson@ogc.org>
2025-12-19 23:10:34 +1100

    input description example and context

--- build.sh
+++ build.sh
@@ -11,4 +11,4 @@ if [ -f '.volumes' ]; then
 fi
 docker run --pull=always --rm --workdir /workspace -v "$(pwd):/workspace" ${VOLUMES} \
   ghcr.io/opengeospatial/bblocks-postprocess \
-  --clean true --base-url http://localhost:9090/register/
+  --clean true --base-url http://localhost:9090/register/ $@
